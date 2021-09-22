676725f24ce29e56e80146e4b9c3785adc476e24
lineman60 <snyder60>
2021-09-21 23:18:54 -0500

    first commit

---
+++ itter.go
@@ -0,0 +1,19 @@
+package integers
+
+import "testing"
+
+const repeateCount = 5
+
+func Repeat(character string) string{
+	var repeated string
+	for i := 0; i < repeateCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
+
+func BenchmarkRepeate (b *testing.B){
+	for i :=0; i <b.N; i++{
+		Repeat("a")
+	}
+}
---
+++ itter_test.go
@@ -0,0 +1,13 @@
+package integers
+
+import "testing"
+
+func TestRepeat(t *testing.T){
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected{
+		t.Errorf("ex[ected %q but got %q", expected, repeated)
+	}
+}
+
