15252b66bb408192444d41cf3a1876761846303b
lineman60 <snyder60>
2021-09-27 22:18:20 -0500

    with triangles

--- strct_test.go
+++ strct_test.go
@@ -13,25 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got,want)
-		}
+	areaTests := []struct{
+		name string
+		shape Shape
+		hasArea float64
+	}{
+		{"Rectangle",Rectangle{12,6}, 72.0},
+		{"Circle",Circle{10},314.1592653589793},
+		{"Triangle",Triangle{12.0, 6.0},36.0},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793 )
-	})
-	t.Run("triangle", func(t *testing.T){
-		triangle := Triangle{12.0, 6.0}
-		checkArea(t, triangle, 36.0)
-	})
-
+	for _, tt := range areaTests{
+		t.Run(tt.name, func(t *testing.T){
+			got := tt.shape.Area()
+			if got != tt.hasArea{
+				t.Errorf("%#v got %g want %g", tt.shape, got,tt.hasArea)
+			}
+		})
+	}
 }
